85fa0a1ac60d5e8b957e1806009b31a7090b878e
Mohamed Mounir <mohammedmounir.dev@gmail.com>
2026-08-19 05:57:25 +0300

    Commit after stash

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,12 @@
+<<<<<<< Updated upstream
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("new featuer will be add!");
 
 
 
 
+=======
+﻿Console.WriteLine("now we made first tag 555 ");
+Console.WriteLine("trying stash  ");
+Console.WriteLine("testing is commit can saves after chang brance  ");
+>>>>>>> Stashed changes
